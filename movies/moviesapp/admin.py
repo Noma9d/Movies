c0c926fb0929b65f4b7f33a87e78db17b092c710
Noma9d <woody0740@gmail.com>
2025-07-21 16:14:37 +0300

    add dark theme

--- movies/moviesapp/admin.py
+++ movies/moviesapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import  Picture, ScreeList, TorrentFile, Tag, Actor, Record
 # Register your models here.
+
+
+admin.site.register(Picture)
+admin.site.register(ScreeList)
+admin.site.register(TorrentFile)
+admin.site.register(Tag)
+admin.site.register(Actor)
+admin.site.register(Record)
